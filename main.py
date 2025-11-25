9f80b93b3f4825553488c13abc5ec945d5bec881
shivesh_j <shivesh.jhunjhunwala@challoners.org>
2025-11-25 15:39:33 +0000

    lesson 6- used geeksfor geeks for split function

--- main.py
+++ main.py
@@ -28,12 +28,18 @@ def view_available_pets(choice,pets_df):
     print(temp_pets_df)
     #print(f"inside veiw_available_pets {choice}")
 
-def registration():
-     name = input("full name (must be at least 2 words)")
-     home_type =  input("Home type (Flat, House, or Farm only)")
-     experience = input("Experience level (None, Some, or Expert)")
-     pet_size = input("Preferred pet size (Small, Medium, Large, or Any)")
-     energy_level = input("Preferred energy level (Low, Medium, High, or Any)")
+def registration(adopters_df):
+    name = input("full name (must be at least 2 words)")
+    home_type =  input("Home type (Flat, House, or Farm only)")
+    experience = input("Experience level (None, Some, or Expert)")
+    pet_size = input("Preferred pet size (Small, Medium, Large, or Any)")
+    energy_level = input("Preferred energy level (Low, Medium, High, or Any)")
+    if len(name.split(" ")) > 0 and home_type in ['flat', 'house', 'farm'] and  experience in ['none', 'some', 'expert'] and pet_size in ['small', 'medium', 'large', 'any'] and energy_level in ['low' , 'medium' , 'high' , 'any']:
+        
+    else:
+        print('false')
+         
+
 
 
 def adopter_login(adopters_df):
@@ -56,15 +62,17 @@ def main():
     pets_df = pets_csv()
     adopters_df = adopters_csv()
     choice = main_menu()
-    while choice!= 5:
-        if choice == '1':    
-            view_available_pets(1, pets_df)
+    if choice == '1':    
+        view_available_pets(1, pets_df)
         #print(f"inside main {choice}")
-        if choice == '2':
-            registration()
-        if choice == '3':
-            adopter_login(adopters_df)
-        if choice == '4':
-            staff_menu()
+    if choice == '2':
+        registration()
+    if choice == '3':
+        adopter_login(adopters_df)
+    if choice == '4':
+        staff_menu()
+    if choice == '5':
+        print("Stopping program...")
+        quit()
     
 main()
